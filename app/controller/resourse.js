28134f3e596408bb72705f309863b9f50040dac5
JXY001A <JXY001A@aliyun.com>
2017-05-07 17:05:45 +0800

    增加了WebGL资源请求路径，以及博客路由

--- app/controller/resourse.js
+++ app/controller/resourse.js
@@ -3,25 +3,33 @@
  * @authors: JXY001A
  * @Emali:   JXY001A@aliyun.com
  * @date:    2017-05-07 12:25:29
- * @desc:    (本地静态资源处理)
+ * @desc:    (WebGL本地静态资源处理)
  * @github:  github.com/JXY001A
  * @version: 1.0
  */
 
-exports.image1 = function(req , res) {
-	var publicPath ="../../public/images/";
-	res.attachment(publicPath+'earth_surface_2048.jpg');
+exports.image = function(req, res) {
+	// 通过参数获取文件名
+	var fileName = req.params.name;
+	// 设置参数
+    var options = {
+    	// 文件资源的相对路径，如果这里不设置，那么faleName就需要绝对路径
+        root: './public/images',
+        // 设置是否提供隐藏文件
+        dotfiles: 'deny',
+        // 设置http头部
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
+    // 将文件发送出去
+    res.sendFile(fileName, options, function(err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
 }
-exports.image2 = function(req , res) {
-	var publicPath ="../../public/images/";
-	res.attachment(publicPath+'earth_normal_2048.jpg');
-}
-exports.image3 = function(req , res) {
-	var publicPath ="../../public/images/";
-	res.attachment(publicPath+'earth_specular_2048.jpg');
-}
-exports.image4 = function(req , res) {
-	var publicPath ="../../public/images/";
-	res.attachment(publicPath+'earth_clouds_1024.png');
-}
-
--- routes/router.js
+++ routes/router.js
@@ -10,19 +10,19 @@
 module.exports = function(app) {
 	// 首页逻辑处理模块
 	var Index = require('../app/controller/index.js');
-	// 加载静态资源处理里模块
+	// 加载webGL静态资源处理模块
 	var Resourse = require('../app/controller/resourse.js');
+	// 博客l路基处理模块控制
+	var Blog = require('../app/controller/blog.js');
+
 	// 首页控制逻辑
 	app.get('/',Index.index);
 
+	// 博客路由控制
+	app.get('/blog/index' , Blog.blogIndex);
 
-
-
-
-	// 静态模块资源
-	app.get('/webGL/img1',Resourse.image1);
-	app.get('/webGL/img2',Resourse.image2);
-	app.get('/webGL/img3',Resourse.image3);
-	app.get('/webGL/img4',Resourse.image4);
+	
+	// webGL静态资源加载路径
+	app.get('/webGL/:name', Resourse.image);
 
 }
