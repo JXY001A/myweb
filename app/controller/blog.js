0b9d8b89c128e7d19da75d19ab3f2ab1cfa4126e
JXY001A <JXY001A@aliyun.com>
2017-05-07 17:09:24 +0800

    blog路基控制代码

---
+++ app/controller/blog.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @authors: JXY001A
+ * @Emali:   JXY001A@aliyun.com
+ * @date:    2017-05-07 16:02:46
+ * @desc:    (博客逻辑控制模块)
+ * @github:  github.com/JXY001A
+ * @version: 1.0
+ */
+
+exports.blogIndex = function(req,res) {
+	res.render('blog');
+}
