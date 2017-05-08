67a159e3b2aa6d4d2530927918f8a346bd561eeb
JXY001A <JXY001A@aliyun.com>
2017-05-08 22:23:38 +0800

    增加视图

---
+++ app/controller/user.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @authors: JXY001A
+ * @Emali:   JXY001A@aliyun.com
+ * @date:    2017-05-07 17:17:40
+ * @desc:    (用户逻辑控制模块)
+ * @github:  github.com/JXY001A
+ * @version: 1.0
+ */
+exports.signIn = function (req,res) {
+	res.render('signIn');
+}
+exports.signUp = function (req,res) {
+	res.render('signUp');
+}
