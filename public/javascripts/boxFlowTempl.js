d3b2004eaed3b990b85cffd54bc098b1fcf5cec1
JXY001A <JXY001A@aliyun.com>
2017-05-11 22:08:39 +0800

    添加flowBox代码和控制代码

--- public/javascripts/boxFlowTempl.js
+++ public/javascripts/boxFlowTempl.js
@@ -1,7 +1,7 @@
 $(function() {
 	// 默认制定的flowBox的宽度
 	var width = 230;
-	// 每一个flowBox的间距
+	// 每一个flowBox的间距(默认指定值)
 	var space = 20;
 	var outerWidth = width + space;
 	// 得到防止flowBox 的外层盒子
@@ -33,18 +33,22 @@ $(function() {
 			page: pageNum
 		}, function(data) {
 			if (data == 'finished') {
-				$(window).unbind('scroll').bind('scroll',function(){
+				// 数据加载完成后的处理
+				$(window).unbind('scroll').bind('scroll', function() {
 					var viewScreenTop = $(window).scrollTop() + $(window).innerHeight();
 					var flowBoxHeight = waterContainer.offset().top + arrTop[getMinIndex()];
-					$(showLoadDom.find('#imgBox')).css('display','none');
+					$(showLoadDom.find('#imgBox')).css('display', 'none');
 					showLoadDom.html('<div>兄弟别扯了，到底了</div>');
-					if (viewScreenTop > flowBoxHeight) {
-						showLoadDom.css('display','block');
-					}else{
-						showLoadDom.css('display','none');
+					// 加30的目的是为了防止在单列的情况下可视区域的高度
+					// 最大只与flowBoxHeight相等，使得结束句不能正常展示的处理
+					if (viewScreenTop + 30 > flowBoxHeight) {
+						showLoadDom.css('display', 'block');
+					} else {
+						showLoadDom.css('display', 'none');
 					}
 				});
 			} else {
+				// 将加载到的数据填充到flowBox的外层盒子中
 				waterContainer.append(data);
 				// 得到本批次添加进来的flowBox
 				var flowDoms = $('.flowBox' + pageNum);
@@ -54,12 +58,12 @@ $(function() {
 						// 获取高度最低的位置索引
 						var minIndex = getMinIndex();
 						var boxHeight = $(el).outerHeight();
-						// console.log(boxHeight);
 						$(el).animate({
 								left: arrLeft[minIndex],
 								top: arrTop[minIndex]
 							},
 							'slow');
+						// 更新记录高度的数组
 						arrTop[minIndex] += boxHeight + space;
 						// 当加载进来的flowBoxs中的最后一个中的img标签将图片加载完成之后
 						// 在开放滚动数据加载加载
@@ -70,7 +74,7 @@ $(function() {
 							// flowBoxs,同时这样更节省性能
 							Layer.layerinit('.flowBox' + pageNum);
 							// 当整个数据加载渲染完成后隐藏加载状态图
-							showLoadDom.css('display','none');
+							showLoadDom.css('display', 'none');
 						}
 					});
 				});
@@ -79,7 +83,14 @@ $(function() {
 	}
 	// 计算容器所能放置的盒子数
 	function getCells() {
-		cells = Math.floor(waterContainer.width() / outerWidth);
+		// 如果屏幕宽度小于790时，flowBox每行只设置1个
+		if ($(window).width() < 790) {
+			cells = 1;
+		} else {
+			// 其他情况依据正常情况处理
+			cells = Math.floor(waterContainer.width() / outerWidth);
+		}
+		return cells;
 	}
 	// 初始化位置记录的数组
 	function initPositionArr() {
@@ -104,11 +115,40 @@ $(function() {
 	function scrollLoadData(e) {
 		var viewScreenTop = $(window).scrollTop() + $(window).innerHeight();
 		var flowBoxHeight = waterContainer.offset().top + arrTop[getMinIndex()];
-		if (viewScreenTop > flowBoxHeight) {
+		if (viewScreenTop + 30 > flowBoxHeight) {
 			pageNum++;
 			loadData();
 		}
 	}
+	// 窗口改变时重新调整flowBox的结构方法
+	function flowBoxAdopt() {
+		var flowBoxs = $('.flowBox');
+		flowBoxs.each(function(index, el) {
+			var minIndex = getMinIndex();
+			var boxHeight = $(el).outerHeight();
+			// console.log(boxHeight);
+			$(el).animate({
+					left: arrLeft[minIndex],
+					top: arrTop[minIndex]
+				},
+				'slow');
+			arrTop[minIndex] += boxHeight + space;
+		});
+	}
+
+	// 窗口大小改变时调整结构
+	$(window).bind('resize', function(event) {
+		// 判断有没有必要调整机构（cells不变不调整，反之亦然）
+		var cellsOld = cells;
+		cells = getCells();
+		if (cells == cellsOld) {
+
+		} else {
+			initPositionArr();
+			flowBoxAdopt();
+		}
+	});
+
 	// 默认首页进入时加载一次
 	loadData();
 
