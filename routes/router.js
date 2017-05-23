983f2713b81da344c619e822ccfba71de2fef7db
JXY001A <JXY001A@aliyun.com>
2017-05-23 12:59:19 +0800

    增加验证路由

--- routes/router.js
+++ routes/router.js
@@ -16,7 +16,9 @@ module.exports = function(app) {
 	var Blog = require('../app/controller/blog.js');
 	// 用户模块
 	var User = require('../app/controller/user.js');
-
+	// 后台逻辑控制模块
+	var Admian = require('../app/controller/admain.js');
+	var Category = require('../app/controller/category.js');
 	// 首页控制逻辑
 	app.get('/',Index.index);
 
@@ -25,6 +27,10 @@ module.exports = function(app) {
 	app.get('/blog/essays' , Blog.blogEssays);
 	app.get('/blog/:id' , Blog.essayShow);
 	app.get('/blog/essay/Uplaod', Blog.essayUpload);
+	app.get('/blog/essayModify/:id',Blog.essayModify);
+	app.get('/blog/asdf/category',Category.showCategories);
+	// 添加分类路由
+	app.post('/blog/essay/addCategory', Blog.addCategory);
 
 	/*用户路由控制*/
 	// 用户登录页
@@ -49,6 +55,18 @@ module.exports = function(app) {
 	app.get('/user/signUp' , User.signUp);
 	// 用户注册信息处理
 	app.post('/user/signUpMess' , User.signUpMess);
+	app.get('/user/messModify' , User.userMessModify);
+	app.post('/user/modifyMessSave', User.saveUserPicture,User.saveUserMess);
+	// 用户博文上传
+	app.post('/user/essay/upload', User.essayUpload);
+
+	// 后台管理路由
+	app.get('/admain/index',Admian.admainIndex);
+	app.get('/admain/addData',Blog.addData);
+	app.post('/admain/blog/modify',Admian.blogModify);
+	// 分页返回blog数据
+	app.post('/admain/blog/page',Admian.showBlogItem);
+	app.delete('/admain/blog/delete',Admian.deleteBlog);
 
 	// webGL静态资源加载路径
 	app.get('/webGL/:name', Resourse.image);
