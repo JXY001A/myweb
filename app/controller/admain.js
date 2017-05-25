dd0fe655f203dc5ef201a3f9647f4d87f41f2289
JXY001A <JXY001A@aliyun.com>
2017-05-25 16:12:48 +0800

    后台管理逻辑增加数据排序

--- app/controller/admain.js
+++ app/controller/admain.js
@@ -2,6 +2,7 @@
 var Essay = require('../modules/Eassy.js');
 var User = require('../modules/User.js');
 var _underscore = require('underscore');
+var Category = require('../modules/category.js');
 // 每一页的显示数量
 var pageNum = 5;
 exports.admainIndex = function(req, res) {
@@ -54,12 +55,32 @@ exports.blogModify = function(req, res) {
 		var blog = req.body;
 		var essayId = blog.essayId;
 		Essay.findById(essayId, function(err, result) {
+			// 再将修改后的信息保存之前首先查询到文章上传时的分类信息
+			Category.findById(result.category, function(err, category) {
+				// 循环类型中保存的essays的id来找到当前修改文章的id并删除之
+				if (category) {
+					category.essaies.forEach(function(o, index) {
+						if (essayId == o.toString()) {
+							category.essaies.splice(index+1,1);
+						}
+					});
+				}
+			});
+			// 将上传的数据整合为一个新的数据结构_essay
 			_essay = _underscore.extend(result, blog);
-			_essay.save(function(err, result) {
+			// 保存整合好的数据
+			_essay.save(function(err, essay) {
 				if (err) {
 					console.log(err);
 				}
-				res.redirect('/blog/' + result._id);
+				// 保存完成后将文章添加到对应的分类中
+				Category.findById(essay.category,function(err,category){
+					category.essaies.push(essay._id);
+					category.save(function(err,cate){
+						console.log('Success');
+					});
+				});
+				res.redirect('/blog/' + essay._id);
 			});
 		});
 	}
